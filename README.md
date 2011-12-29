effda0393f476ab101a640f459ccff325856247b
Abid Din <hello@craftedpixelz.co.uk>
2011-12-29 13:52:10 +0000

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# jqInstagram - Showcase your Instagram photos using jQuery #
+
+
+## About ##
+
+jqInstagram is a simple jQuery plugin that enables you to display your latest Instagram photos on your website or blog.
+
+##Usage ##
+
+### HTML: ###
+
+    <ul id="instagram"></ul>
+
+Create an empty unordered list and give it an ID of choice.
+
+### CSS: ###
+
+    #instagram {
+    	margin:0;
+    	padding:0;
+    	overflow:hidden;
+    }
+    
+    #instagram li {
+    	list-style:none;
+    	float:left;
+    	margin:0 20px 20px 0;
+    }
+
+These are the most basic styles necessary to create a horizontal list of images.
+
+### Scripts/Initialisation: ###
+
+    <script src="http//code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="jqinstagram.js"></script>
+    
+    <script>
+    $("#instagram").jqinstagram({
+      "user_id": "Your User ID",
+      "access_token": "Your Access Token",
+      "count": 5
+    });
+    </script>
+
+The Instagram API requires 2 pieces of information to display photos from a user feed. These are the User ID and an Access Token. How do you get these, you say? Don't worry, I've created a nifty tool that will allow you to obtain both (see website docs).
+
+
+## License ##
+
+Copyright (c) 2011 Crafted Pixelz
+Licensed under the MIT license
---
+++ js/jqinstagram.js
@@ -0,0 +1,47 @@
+/*
+ * jqinstagram
+ * Created by: Abid Din - http://craftedpixelz.co.uk
+ * Version: 1.0
+ * Copyright: Crafted Pixelz
+ * License: MIT license
+ * Updated: 30th December 2011
+*/
+ 
+(function ($) {
+    $.fn.jqinstagram = function (options) {
+
+        // Defaults
+        var defaults = {
+            "user_id": null,
+            "access_token": null,
+            "count": 10
+        };
+
+        var o = $.extend(defaults, options);
+
+        return this.each(function () {
+        
+          // Vars
+          var elem = $(this),
+              url = "https://api.instagram.com/v1/users/" + o.user_id + "/media/recent?access_token=" + o.access_token + "&count=" + o.count + "&callback=?";
+          		
+            // Get the images	
+            $.getJSON(url, function (data) {
+                $.each(data.data, function (i, val) {
+                    var li = $("<li/>").appendTo(elem),
+                        a = $("<a/>", {"href": val.link, "target": "_blank"}).appendTo(li),
+                        img = $("<img/>", {"src": val.images.thumbnail.url}).appendTo(a);
+                    
+                    if(val.caption){
+                        img.attr("title", val.caption.text);
+                    }
+                });
+            });
+            
+            if(o.user_id == null || o.access_token == null){
+              elem.append("<li>Please specify a User ID and Access Token, as outlined in the docs.</li>");
+            }
+
+        });
+    };
+})(jQuery);
